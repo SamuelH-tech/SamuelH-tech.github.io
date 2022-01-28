9add20a221a891e017465b2b0b8736b3dd940066
Samuel Hasham <samuel.hasham@dcmail.ca>
2022-01-28 18:20:22 -0500

    Added a working contact list and subscription option on the contact page.

--- scripts/app.js
+++ scripts/app.js
@@ -1,109 +1,152 @@
-// IIFE -- Immediatly Invoked Function Express
-// AKA anon self-executing function
+// IIFE -- Immediately Invoked Function Express
+// AKA anonymous self-executing function
+
+"use strict";
 (function()
 {
-    
-    function DisplayHomePage()
+    function DisplayHome()
     {
-        
         console.log("Home Page");
+
         let AboutUsButton = document.getElementById("AboutUsButton");
         AboutUsButton.addEventListener("click", function()
         {
-            location.href="about.html";
+            location.href = "about.html";
         });
-        console.log(AboutUsButton);
 
-        // Step 1 - get a reference to an entry point(s) (insertion / deletion)
+        // Step 1 - get a reference to an entry point(s) (insertion / deletion point)
         let MainContent = document.getElementsByTagName("main")[0];
-        let documentBody = document.body;
-        //step 2 - Create a HTML element in memory
+        let DocumentBody = document.body;
+        
+        // Step 2 - Create a HTML Element in memory
         let MainParagraph = document.createElement("p");
         let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the article paragraph.</p>`;
-        //step 3 - Configure the new element
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+        // Step 3 - Configure new Element
         MainParagraph.setAttribute("id", "MainParagraph");
         MainParagraph.setAttribute("class", "mt-3");
-        //MainParagraph.textContent = "This is the Main Paragraph!";
         let FirstString = "This is";
-        let SecondString = `${FirstString} The Main Paragraph`;
+        let SecondString = `${FirstString} the Main Paragraph`;
         MainParagraph.textContent = SecondString;
         Article.setAttribute("class", "container");
-        //step 4 Perform Insertion / deletion
 
 
-        //example of insert after / append.
+        // Step 4 - perform insertion / deletion
+
+        // example of insert after (append)
         MainContent.appendChild(MainParagraph);
         Article.innerHTML = ArticleParagraph;
-        documentBody.appendChild(Article);
-        // documentBody.innerHTML = `
-        //     <div class="container">
-        //     <h1 class="display-1">Hello, World!</h1>
-        //     <p class = "mt-5">and... what do you think of this!</p>
-        // `;
+        DocumentBody.appendChild(Article);
 
-        //example of insert before
-        //MainContent.before(MainParagraph);
+    }
 
-        //Example of deletion
-        //document.getElementById("AboutUsButton").remove();
-
+    function DisplayAboutPage()
+    {
+        console.log("About Us Page");
     }
 
     function DisplayProductsPage()
     {
-        console.log("Products Page");
+        console.log("Our Projects Page");
     }
+
     function DisplayServicesPage()
     {
-        console.log("Services Page");
-        
+        console.log("Our Services Page");
     }
-    function DisplayAboutPage()
-    {
-        console.log("About Page");
-        
-    }
+
     function DisplayContactPage()
     {
-        console.log("Contact Page");
-        
+        console.log("Contact Us Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function()
+        {
+            if(subscribeCheckbox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
     }
 
+    function DisplayContactListPage()
+    {
+        console.log("Contact-List Page");
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = ""; // data container -> add deserialized data from the localStorage
+
+            let keys = Object.keys(localStorage); // returns a string array of keys
+
+            let index = 1; // counts how many keys
+
+            // for every key in the keys array (collection), loop
+            for (const key of keys) 
+            {
+                let contactData = localStorage.getItem(key); // get localStorage data value related to the key
+
+                let contact = new Contact(); // create a new empty contact object
+                contact.deserialize(contactData);
+
+                // inject a repeatable row into the contactList
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>
+                `;
+
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
+    }
+
+
+    // named function
     function Start()
     {
-        console.log("App Started!");
+        console.log("App Started!!");
 
-        switch(document.title)
-        {
-            case "Home":
-                DisplayHomePage();
-                break;
-            case "Products":
-                DisplayProductsPage();
-                break;
-            case "Services":
-                DisplayServicesPage();
-                break;
-            case "About us":
-                DisplayAboutPage();
-                break;
-            case "Contact us":
-                DisplayContactPage();
-                break;
-
+        switch (document.title) {
+          case "Home":
+            DisplayHome();
+            break;
+          case "About Us":
+            DisplayAboutPage();
+            break;
+          case "Our Products":
+            DisplayProductsPage();
+            break;
+          case "Our Services":
+            DisplayServicesPage();
+            break;
+          case "Contact-List":
+            DisplayContactListPage();
+            break;
+          case "Contact Us":
+            DisplayContactPage();
+            break;
         }
-
-       
-
-
-
-
     }
+    
 
     window.addEventListener("load", Start);
-
-
 
 
 })();
